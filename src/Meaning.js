d5230b85f8c1d551b422c95926a5a1b17ebc5e88
Faith Osabutey <osabuteyfaith@gmail.com>
2025-01-15 17:50:39 +0000

    Added synonyms of the searched words

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,12 +1,20 @@
 import React from "react";
+import Synonyms from "./Synonyms";
+import Example from "./Example";
 
 export default function Meaning(props) {
-  //console.log(props.meaning);
+  console.log(props.meaning.partOfSpeech);
   return (
     <div className="Meaning">
       <h4>{props.meaning.partOfSpeech}</h4>
-      <p>{props.meaning.definition}</p>
-      <p>{props.meaning.example}</p>
+      <p>
+        <strong>Definition: </strong>
+        {props.meaning.definition}
+      </p>
+
+      <Example example={props.meaning.example} />
+
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
