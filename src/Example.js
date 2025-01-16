b0e314e95a4351c2c01a7127d6adf8ea30c4a350
Faith Osabutey <osabuteyfaith@gmail.com>
2025-01-16 03:49:58 +0000

    Made some finishing touches

--- src/Example.js
+++ src/Example.js
@@ -4,6 +4,7 @@ export default function Example(props) {
   if (props.example) {
     return (
       <div className="Example">
+        <strong>example: </strong>
         <em> {props.example}</em>
       </div>
     );
